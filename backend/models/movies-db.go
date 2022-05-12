a4f0148915908a5ae7f7a9eddd38dfb06bee43a5
Joe Blasingame <jblsngm@gmail.com>
2022-05-11 22:49:06 -0500

    end lesson 44

--- backend/models/movies-db.go
+++ backend/models/movies-db.go
@@ -16,8 +16,8 @@ func (m *DBModel) Get(id int) (*Movie, error) {
 	defer cancel()
 
 	query := `select id, title, description, year, release_date, rating, runtime, mpaa_rating,
-							created_at, updated_at from movies where id = $1
-	`
+              created_at, updated_at from movies where id = $1
+  `
 
 	row := m.DB.QueryRowContext(ctx, query, id)
 
@@ -41,8 +41,33 @@ func (m *DBModel) Get(id int) (*Movie, error) {
 	}
 
 	// get the genres
-	query = `select `
+	query = `select
+              mg.id, mg.movie_id, mg.genre_id, g.genre_name
+            from
+              movies_genres mg
+              left join genres g on (g.id = mg.genre_id)
+            where
+              mg.movie_id = $1
+  `
 	rows, _ := m.DB.QueryContext(ctx, query, id)
+	defer rows.Close()
+
+	var genres []MovieGenre
+	for rows.Next() {
+		var mg MovieGenre
+		err := rows.Scan(
+			&mg.ID,
+			&mg.MovieID,
+			&mg.GenreID,
+			&mg.Genre.GenreName,
+		)
+		if err != nil {
+			return nil, err
+		}
+		genres = append(genres, mg)
+	}
+
+	movie.MovieGenre = genres
 
 	return &movie, nil
 }
